7a4b14df8f34e6207e781105b7828c8972c78929
Abrar Faiyaz <abrarsamin04@gmail.com>
2026-09-12 08:08:59 +0600

    Configured female_sprite_sheet_v2.png as primary female player sprite sheet

--- core/src/main/java/com/infectedhour/core/screens/GameScreen.java
+++ core/src/main/java/com/infectedhour/core/screens/GameScreen.java
@@ -510,7 +510,10 @@ public class GameScreen implements Screen {
         idleFrameHeight = idleTexture.getHeight() / 4;
         idleFrames = TextureRegion.split(idleTexture, idleFrameWidth, idleFrameHeight);
 
-        femalePlayerTexture = loadTextureSafely("female/female_sprite_sheet_v2.jpg");
+        femalePlayerTexture = loadTextureSafely("female/female_sprite_sheet_v2.png");
+        if (femalePlayerTexture == null) {
+            femalePlayerTexture = loadTextureSafely("female/female_sprite_sheet_v2.jpg");
+        }
         if (femalePlayerTexture == null) {
             femalePlayerTexture = loadTextureSafely("female/female_sprite_sheet_v3.png");
         }
